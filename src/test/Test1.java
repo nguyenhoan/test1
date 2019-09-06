e188df6de709fa7cde24d138dca2aed20cb2830e
Hoan Nguyen <hoanamzn@amazon.com>
2019-09-05 18:13:12 -0700

    Update Test1.java

--- src/test/Test1.java
+++ src/test/Test1.java
@@ -4,8 +4,20 @@ class Test1 {
     void m1() {
         foo();
     }
-    
+
     void m2() {
+        bar();
+    }
+
+    void m3() {
+        bar();
+    }
+
+    void m4() {
+        bar();
+    }
+
+    void m5() {
         bar();
     }
 }
